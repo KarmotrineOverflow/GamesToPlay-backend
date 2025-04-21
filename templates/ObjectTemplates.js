807d0a0a10243b46347eed07bac652598dfa396f
Elfie Marvin John Campit <elfiemarvinjohncampit@gmail.com>
2025-04-21 20:37:27 +0800

    Added object templates

---
+++ templates/ObjectTemplates.js
@@ -0,0 +1,15 @@
+class Game {
+
+    constructor(title, gameDescription, thoughts, boxArt) {
+
+        this.title = title;
+        this.gameDescription = gameDescription;
+        this.thoughts = thoughts
+        this.boxArt = boxArt
+    }
+}
+
+module.exports = {
+
+    Game
+}
